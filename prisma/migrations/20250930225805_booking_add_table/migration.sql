70ffcf1442167ea83ef385345d1b171d866b04f3
tumi267 <itu.matlala4@gmail.com>
2025-10-02 02:39:24 +0200

    calendar markup

---
+++ prisma/migrations/20250930225805_booking_add_table/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "bookings" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "bookings_pkey" PRIMARY KEY ("id")
+);
--- src/app/components/(public)/booking/Clendar/Calender.tsx
+++ src/app/components/(public)/booking/Clendar/Calender.tsx
@@ -6,10 +6,48 @@ interface Props {
     data:any
   }
 function Calender({select,viewNum,viewselected,data}:Props) {
+  const date=new Date()
+  let year=date.getFullYear()
+  let month=date.getMonth() 
+  let day=date.getDate()
+  let endMouthdate=new Date(year, month + 1, 0).getDate();
+  let daysOfMonth = new Array(endMouthdate).fill(null)
+  .map((_, i) => i + 1)
+  
   return (
     <div>
+      <h3>{day}/{month + 1}/{year}</h3>
+      
         <h2>Select Date</h2>
-        <input type="date" onChange={(e)=>{select({...data,date:e.target.value})}}/>
+        <div className="w-[80%] mx-auto">
+        <div className="grid grid-cols-7 text-center border-b font-semibold">
+          <div className="p-2">Sun</div>
+          <div className="p-2">Mon</div>
+          <div className="p-2">Tue</div>
+          <div className="p-2">Wed</div>
+          <div className="p-2">Thu</div>
+          <div className="p-2">Fri</div>
+          <div className="p-2">Sat</div>
+        </div>
+        <div className="grid grid-cols-7 gap-0">
+          {daysOfMonth.map((d, i) => {
+            const isToday = d === day; // check if current date
+            return (
+            <div
+            key={i}
+            className={`min-h-[5em] p-2 box-border border border-black text-left ${
+              isToday ? "text-[red] font-bold" : ""
+              }`}
+              >
+            <div className="text-right">
+              <p>{d}</p>
+            </div>
+          </div>
+          );
+          })}
+        </div>
+        </div>
+        {/* <input type="date" onChange={(e)=>{select({...data,date:e.target.value})}}/> */}
         <br/>
         <button onClick={()=>{viewNum(viewselected+1)}}>next</button>
     </div>
