70bf1f68de10975d9529b1889123e7b9fd360550
tumi267 <itu.matlala4@gmail.com>
2025-10-29 13:38:12 +0200

    pro page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import '../globals.css';
 import { ReactNode } from "react";
 import Nav from '../components/admin/nav/Nav';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
